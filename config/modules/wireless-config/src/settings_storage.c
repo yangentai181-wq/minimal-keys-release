ed12ea469d3b5e3a7ab1482275b6937b5a18e7ac
iwanedaijun <yangentai181@users.noreply.github.com>
2026-04-13 17:08:56 +0900

    fix: SYS_INIT requires literal priority value

--- config/modules/wireless-config/src/settings_storage.c
+++ config/modules/wireless-config/src/settings_storage.c
@@ -102,6 +102,6 @@ int wireless_config_settings_save(void)
     return 0;
 }
 
-SYS_INIT(wireless_config_settings_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY + 1);
+SYS_INIT(wireless_config_settings_init, APPLICATION, 91);
 
 #endif /* CONFIG_WIRELESS_CONFIG_PERSIST_SETTINGS */
